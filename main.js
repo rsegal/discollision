00624f72fe4963a6beeaf59dc1eaa43bc8eacf6f
Liran Sharir <lsharir@gmail.com>
2013-01-23 00:11:06 -0500

    updater, interval

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-ctx.font = "60px Arial";
-ctx.textAlign = "center";
-ctx.fillText("Hello World!", 200, 200);
+var i=0;
+//canvas.addEventListener('keydown',onKeyDown,false);
+
+var updater = function () {
+	ctx.clear;
+	i++;
+	ctx.fillStyle = "#0000FF";
+	ctx.fillRect(50+(i % 50),150,50,100);
+	console.log((i % 50));
+	console.log("updater");
+	
+}
+
+//var onKeyDown(event) {
+//	console.log('keyCode: '+event.keyCode);
+//}
+var interval= 33;
+intervalID = setInterval(updater,interval);
