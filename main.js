cfeadcf6255eb2ad6369dfcc677843d921a8d3d1
Raphael Segal <rsegal@andrew.cmu.edu>
2013-01-27 23:24:13 -0500

    Revert "stable version, burner and all arrows work, simplistic physics, flags are in the code but are buggy, and the players are supposed to be rendering, but aren't."
    This reverts commit c109655d99fcfd6c05d6d3809df3780497ef12cc.

--- main.js
+++ main.js
@@ -3,43 +3,8 @@ function dbPrint(msg) {
 	console.log(msg);
     }
 }
-//var score = 
 
-var flag = function(initX,initY,player,enemy,color) {
-    this.homeX = initX;
-    this.homeY = initY;
-    this.player = player;
-    this.color = color;
-    this.enemy = enemy;
-    this.stolen = false;
-    this.drawFlag = function(viewer) {
-	var x;
-	var y;
-	if (self.stolen === true) {
-	    dbPrint(this.player.ID + "'s flag is being carried at (" + this.enemy.x+ "," + this.enemy. y+ ").")
-	    x = this.enemy.x;
-	    y = this.enemy.y;
-	}
-	else {
-	    x = self.homeX;
-	    y = self.homeY;
-	}
-	dbPrint(self.homeX);
-	dbPrint(self.homeY);
-	dbPrint(this.player.ID + "'s flag is at (" + x + "," + y + ").");
-	renderX = x - viewer.x;
-	renderY = y - viewer.y;
-	dbPrint(viewer.ID + " is (" + renderX + "," + renderY + ") from " + player.ID + "'s flag.");
-	if ((Math.abs(renderX) <= 150) && (Math.abs(renderY) <= 150)) 
-	{
-	    
-	    drawDisk(viwer.x - renderX,viewer.y - renderY,5,this.color); 
-	    // make legit flag art
-	}
-    }
-}
-
-var player = function(initX,initY,viewX,viewY,size,player) {
+var player = function(initX,initY,viewX,viewY,size,playerID) {
     this.x = initX;
     this.y = initY;
     this.vX = 0;
@@ -48,15 +13,9 @@ var player = function(initX,initY,viewX,viewY,size,player) {
     this.aX = 0;
     this.aY = 0;
     this.radius=size;
-    this.ID=player;
-    if (this.ID === "A") { this.color = "rgb(255,0,0)"; }
-    else { this.color = "rgb(0,0,255)"; }
+    this.ID=playerID;
     this.viewX = viewX;
     this.viewY = viewY;
-    this.torch = 0;
-    // delays erasing the torch visual effect
-    this.torchShutoffCounter = 0;
-    this.torchShutoffLimit = 150; // units of 33 milliseconds
     dbPrint(this);
 }
 var drawBG = function(locX,locY,baseX,baseY) {
@@ -71,7 +30,7 @@ var drawEnemy = function(myShip,eShip,centerX,centerY) {
 	var distX=eShip.x-myShip.x;
 	var distY=eShip.y-myShip.y;
 	if (((distX)-eShip.radius<=250) && ((distY)-eShip.radius<=250) && ((distX)+eShip.radius>=-250) && ((distY)+eShip.radius>=-250)) {
-	    drawDisc(distX+centerX,distY+centerY,eShip.radius,eShip.color);
+	    drawDisc(distX+centerX,distY+centerY,eShip.radius);
 		}
 }
 
@@ -102,7 +61,7 @@ var checkWallCollision = function (disc) {
 	if (disc.y+disc.radius >= 3000) {disc.vY=-disc.vY; }
 }
 
-var drawArrow = function (x,y,color,scale) {
+var drawArrow = function (x,y, color) {
     ctx.beginPath();
     ctx.moveTo(x,y);
     ctx.lineTo(x + 40,y+25);
@@ -112,10 +71,10 @@ var drawArrow = function (x,y,color,scale) {
     ctx.lineTo(x - 15,y + 25);
     ctx.lineTo(x - 40,y + 25);
     ctx.closePath();
+    ctx.strokeStyle = color;
+    ctx.stroke();
     ctx.fillStyle = color;
-    //ctx.scale(scale,scale)
     ctx.fill();
-    //ctx.scale(1/scale,1/scale);
 }
 
 var drawPointer = function (x,y,self,enemy) {
@@ -127,7 +86,7 @@ var drawPointer = function (x,y,self,enemy) {
     // ctx.save();
     ctx.translate(self.viewX,self.viewY);
     ctx.rotate(angle);
-    drawArrow(x,y, "rgba(255, 0, 0, 0.5)", 1,1);
+    drawArrow(x,y, "rgba(255, 0, 0, 0.5)");
     ctx.rotate(-angle);
     ctx.translate(-self.viewX,-self.viewY);
 }
@@ -135,13 +94,10 @@ var drawPointer = function (x,y,self,enemy) {
 var drawHeading = function (x,y,self) {
     dbPrint("Drawing " + self.ID + "'s Heading");
     var angle = Math.atan2(self.vX,-self.vY);
-    var speedWeight = 30; // empirical, basically magic
-    var speed = Math.sqrt(Math.pow(self.vX,2) + Math.pow(self.vY,2))
-    var magnitude = speed / speedWeight;
     dbPrint("vX = " + self.vX + ", vY = " + self.vY + ", angle = " + angle);
     ctx.translate(self.viewX,self.viewY);
     ctx.rotate(angle);
-    drawArrow(x,y,"rgba(0, 255, 0, 0.5)",magnitude);
+    drawArrow(x,y,"rgba(0, 255, 0, 0.5)");
     ctx.rotate(-angle);
     ctx.translate(-self.viewX,-self.viewY);
 }
@@ -151,32 +107,8 @@ var drawAcceleration = function(x,y,self) {
     var angle = Math.atan2(self.aX,-self.aY);
     ctx.translate(self.viewX,self.viewY);
     ctx.rotate(angle);
-    dbPrint(self.ID + "'s torch cycle at " + self.torch);
-    if (self.torch < 4) 
-    {
-	ctx.drawImage(burner,0,0,20,50,x-10,y,20,50);
-	self.torch++;
-	console.log(self.torch);
-    }
-    else if (self.torch<8) 
-    {
-	ctx.drawImage(burner,20,0,20,50,x-10,y,20,50);
-	self.torch++;
-	console.log(self.torch);
-    }
-    else if (((self.torch % 8) + 4) < 12) 
-    {
-	ctx.drawImage(burner,40,0,20,50,x-10,y,20,50);
-	self.torch++;
-	console.log(self.torch);
-    }
-    else if (((self.torch % 8) + 4) < 16) {
-	ctx.drawImage(burner,60,0,20,50,x-10,y,20,50);
-	self.torch++;
-	console.log(self.torch);
-    }
-    else {self.torch = 8;}
-    dbPrint("foo");
+    ctx.fillStyle = "rgba(255, 255, 255, 1)";
+    ctx.fillRect(x-10,y,20,50);
     ctx.rotate(-angle);
     ctx.translate(-self.viewX,-self.viewY);
     
@@ -185,25 +117,22 @@ var circle = function(cx,cy,radius) {
 	ctx.arc(cx,cy,radius,0,2*Math.PI, true);
 }
 
-var drawDisc = function(locX,locY,radius,color) {
+var drawDisc = function(locX,locY,radius) {
     ctx.beginPath();
 //    ctx.fillRect(locX,locY,radius,radius)
     ctx.arc(locX,locY,radius,0,2*Math.PI, true);
-    ctx.fillStyle = color;
     ctx.fill();
 }
 
 var updater = function() {
     dbPrint(intervalCounter++);
     dbPrint(keys);
- 
-    // Flush acceleration
+    // Caution, key checks use falsy checking of relevnt keys!
+
     A.aX = 0;
     A.aY = 0;
     B.aX = 0;
     B.aY = 0;
-
-    // Caution, key checks use the default, falsy evaluation! 
 
     // A's movement
     if (keys[65]) {A.aX -= A.a} // A Key
@@ -216,7 +145,7 @@ var updater = function() {
     if (keys[38]) {B.aY -= B.a} // Up Arrow
     if (keys[39]) {B.aX += B.a} // Right Arrow
     if (keys[40]) {B.aY += B.a} // Down Arrow
-    
+
     // Resolve acceleration to new velocity
     A.vX += A.aX;
     A.vY += A.aY;
@@ -230,9 +159,7 @@ var updater = function() {
     B.y+=B.vY;
 
     dbPrint(A);
-    dbPrint(aF);
     dbPrint(B);
-    dbPrint(bF);
 
     //*********calculations before clearing the picture********
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -245,14 +172,8 @@ var updater = function() {
     drawBG(B.x,B.y,650,50);
 
     // Players
-    drawDisc(A.viewX, A.viewY , A.radius, A.color);
-    drawDisc(B.viewX, B.viewY , B.radius, B.color);
-
-    // draws flags on top of players if applicable
-    aF.drawFlag(A);
-    aF.drawFlag(B);
-    bF.drawFlag(A);
-    bF.drawFlag(B);
+    drawDisc(300,300,A.radius);
+    drawDisc(900,300,B.radius);
 
     // Enemies if they should be visible
     drawEnemy(A,B,300,300);
@@ -269,7 +190,7 @@ var updater = function() {
     drawPointer(0,-200,A,B);
     drawPointer(0,-200,B,A);
 
-    var epsilon = 10;
+    var epsilon = 0.000000001
 
     if (Math.sqrt(Math.pow(A.vX,2) + Math.pow(A.vY,2)) >= epsilon) {
 	drawHeading(0,-100,A);
@@ -280,34 +201,14 @@ var updater = function() {
 
     dbPrint("A.aX = " + A.aX + ", A.aY = " + A.aY);
     dbPrint("B.aX = " + B.aX + ", B.aY = " + B.aY);
-    
-    dbPrint("A's torch shutoff: " + A.torchShutoffCounter + "/" + A.torchShutoffLimit);
-    dbPrint("B's torch shutoff: " + B.torchShutoffCounter + "/" + B.torchShutoffLimit);
 
     if (Math.sqrt(Math.pow(A.aX,2) + Math.pow(A.aY,2)) !== 0) {
 	drawAcceleration(0,20,A);
-	A.torchShutoffCounter = 0;
-    }
-    else {
-	if (A.torchShutoffCounter < A.torchShutoffLimit) {
-	    A.torchShutoffCounter++;
-	}
-	else {
-	    A.torch = 0;
-	}
     }
     if (Math.sqrt(Math.pow(B.aX,2) + Math.pow(B.aY,2)) !== 0) {
 	drawAcceleration(0,20,B);
-	B.torchShutoffCounter = 0;
     }
-    else {
-	if (A.torchShutoffCounter < A.torchShutoffLimit) {
-	    B.torchShutoffCounter++;
-	}
-	else {
-	    B.torch = 0;
-	}
-    }
+    
 
 }
 
@@ -320,6 +221,7 @@ function onKeyUp(event) {
     keys[event.keyCode] = 0;
     dbPrint(keys);
 }
+
 
 function main() {
     debugMode = true;
@@ -336,17 +238,11 @@ function main() {
 
     A = new player(2900,100,300,300,10,1);
     B = new player(100,2900,900,300,20,2);
-    aF = new flag(A.x, A.y, A, B, "rgb(100, 0, 0)");
-    bF = new flag(B.x, B.y, B, A, "rgb(0, 0, 100)");
-    dbPrint(aF);
-    dbPrint(bF);
     background = new Image();
     background.src = "./assets/starmap.jpg";
-	burner = new Image();
-	burner.src = "./assets/burner.png";
     dbPrint(background);
     interval= 33;
     intervalID = setInterval(updater, interval);
 }
 
-main();
+main()
